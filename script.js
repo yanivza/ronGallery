b16b000d3957f3c90b867533a595e76b3183112d
yanivza <101286556+yanivza@users.noreply.github.com>
2022-08-06 13:55:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 
+
 const numbers_of_images=12
 
 function one_img_from_array_gallery(num_of_images){
@@ -14,7 +15,7 @@ function make_div_for_img_and_append(img){
     let img_div=document.createElement("div")
     img_div.className="photo_div"
     let img_pc=document.createElement("img")
-    img_pc.src = "/css/img/"+img+".jpg";
+    img_pc.src = "./css/img/"+img+".jpg";
     img_div.append(img_pc)
     bored_photo.append(img_div)
 
